ee910b89b247e16e8d490cfdd4364c732f83b8a8
LuisAMLopez <lucho-1100@hotmail.com>
2018-12-28 19:01:25 +0000

    create app posts

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from platzigram import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('order-numbers/', views.order_numbers)
+    path('order-numbers/', views.order_numbers),
+    path('posts/', include('posts.urls'))
 ]
