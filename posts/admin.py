897636aa3483cfe0b5a5e85964b956948fd4a7df
LuisAMLopez <lucho-1100@hotmail.com>
2019-01-02 17:34:39 +0100

    register posts admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'photo', 'user')
+    list_display_links = ('title',)
+    list_filter = ('created_at', 'updated_at')
+
+    search_fields = (
+        'title',
+        'user__username',
+        'user__first_name',
+        'user__last_name'
+    )
