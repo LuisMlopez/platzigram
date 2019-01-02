54a296a2d335b3d833f1f302245425a5809d9ed5
LuisAMLopez <lucho-1100@hotmail.com>
2019-01-02 17:27:37 +0100

    create post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from users.models import Profile
+
+
+class Post(models.Model):
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{title} by @{username}'.format(
+            title=self.title,
+            username=self.user.username
+        )
